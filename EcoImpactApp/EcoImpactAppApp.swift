8db17074b399356c4143b41e58cf4ed79233c6a5
Abigail <abigailsspaceship@gmail.com>
2024-06-24 14:52:09 -0400

    Initial Commit

---
+++ EcoImpactApp/EcoImpactAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcoImpactAppApp.swift
+//  EcoImpactApp
+//
+//  Created by Abigail Boshart on 6/24/24.
+//
+
+import SwiftUI
+
+@main
+struct EcoImpactAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
